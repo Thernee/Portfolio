a1fc455650f4744d75733c42f6695f483198a660
Sani Abubakar Adam <Athernee@gmail.com>
2025-09-19 11:29:39 +0100

    add more cards

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,14 +2,14 @@ import Header from "./Header"
 import Footer from "./Footer";
 import Food from "./Food";
 import Card from "./Card";
+import SecondCard from "./SecondCard";
 
 function App() {
   return (
     <>
       <Card />
-      <Card />
-      <Card />
-      <Card />
+      {/* <SecondCard /> */}
+      <Footer />
     </>
   );
 }
