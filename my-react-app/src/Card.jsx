678c4b01b23878e1882fb4bc5f999ffa6ee12af2
Sani Abubakar Adam <Athernee@gmail.com>
2025-09-19 11:30:04 +0100

    implement more cards

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -1,10 +1,17 @@
 import profilePic from './assets/profile-pic.png';
 export default function Card() {
   return (
-    <div className="card">
-      <img className='card-image' src={profilePic} alt="profile picture"></img>
-      <h2 className='card-title'>Sani Abubakar Adam</h2>
-      <p className='card-text'>Fullstack Software Developer. Familiar with NodeJs, NestJs, Flask and React</p>
+    <div className='cards'>
+      <div className="card">
+        <img className='card-image' src={profilePic} alt="profile picture"></img>
+        <h2 className='card-title'>Sani Abubakar Adam</h2>
+        <p className='card-text'>Fullstack Software Developer. Familiar with NodeJs, NestJs, Flask and React</p>
+      </div>
+      <div className="card">
+        <img className='card-image' src={profilePic} alt="profile picture"></img>
+        <h2 className='card-title'>Sani Abubakar Adam</h2>
+        <p className='card-text'>A Full-Stack Engineer specializing in Node.js and TypeScript, with hands-on experience building production-style backends and desktop apps. I deliver well-tested APIs, secure authentication flows, and packaged cross-platform Electron apps.</p>
+      </div>
     </div>
   );
 }
